33da222e8ede1548f85e89ea2d30e546fb725c4b
bonoluca <bonoluca0707@gmail.com>
2025-09-30 09:08:46 +0200

    LES30/09/25

--- hfst_2 Dictionaries/oefenmee/oefenmee_10.py
+++ hfst_2 Dictionaries/oefenmee/oefenmee_10.py
@@ -1,36 +1,42 @@
-# Vul eerst aan. Daarna pas uitvoeren!
-dictionary = {"a": 0, "b": 1, "c": 1, "d": 2, "e": 3}
+# # Vul eerst aan. Daarna pas uitvoeren!
+dictionary = {
+    "a": 0, 
+    "b": 1, 
+    "c": 1, 
+    "d": 2, 
+    "e": 3
+    }
 
-""" Geef aan wat volgende code print"""
-" Vul aan: "
-print(dictionary)
+# """ Geef aan wat volgende code print"""
+# " Vul aan:  die print  a: 0, b: 1, c: 1, d: 2, e: 3 "
+# print(dictionary)
 
-" Vul aan: "
-for x in dictionary:
-    print(x)
+# " Vul aan: die print  a, b , c , d , e  "
+# for x in dictionary:
+#     print(x)
 
-" Vul aan: "
-print( list(dictionary.keys()))
+# " Vul aan: die neemt alle sleutels en zet die in een lijst ['a', 'b', 'c', 'd', 'e'] "
+# print( list(dictionary.keys()))
 
-" Vul aan: "
-print( dictionary.get("e", 4))
+# " Vul aan: die neemt e en vrevangt 3 met 4 " #corectie die print 3 omdat die gaat kijken als e in de dictionairy zit als die er niet inzit dan print die 4 
+# print( dictionary.get("e", 4))
 
-" Vul aan: "
+" Vul aan: die neemt alle waarde en zet die in een lijst [0, 1, 1, 2, 3] "
 print( list(dictionary.values()))
 
-" Vul aan: "
-print( dictionary.get("q", 4))
+# " Vul aan: "
+# print( dictionary.get("q", 4))
 
-" Vul aan: "
-for x, y in dictionary.items():
-    print(y, x)
+# " Vul aan: "
+# for x, y in dictionary.items():
+#     print(y, x)
 
-" Vul aan: "
-for x in dictionary.values():
-    print(x)
+# " Vul aan: "
+# for x in dictionary.values():
+#     print(x)
 
-" Vul aan: "
-print( dictionary.pop("c"))
+# " Vul aan: "
+# print( dictionary.pop("c"))
 
-"Vul aan: "
-print( list(dictionary.items()) )
+# "Vul aan: "
+# print( list(dictionary.items()) )
--- hfst_2 Dictionaries/oefenmee/oefenmee_9.py
+++ hfst_2 Dictionaries/oefenmee/oefenmee_9.py
@@ -1,7 +1,54 @@
 # Start de oefen mee met onderstaande dictionary.
+
+
 recept = { # Sleutel is ingredi?nt, waarde is hoeveelheid
     "Aardappelen": 800,
     "Wortelen": 500,
     "erwten": 300,
     "Worsten": 400
 }
+
+print(recept)
+
+# print("Recept voor worst met wortelen en erwten.")
+
+
+# for sleutel, waarde in recept.items():
+#     print(sleutel,waarde)
+
+# Print de zin "Recept voor worst met wortelen en erwten."
+
+# Overloop nu de verschillende ingrediënten van dit recept. Ieder ingrediënt moet apart geprint worden. 
+# De te printen regel is "- *ingrediënt*: *hoeveelheid* gr.
+# De methode items() dient tijdens deze opdracht gebruikt te worden.
+
+# 	In de opdrachtprompt ziet dit er als volgt uit.
+# 	Recept voor worst met wortelen en erwten.	print
+# print
+# print
+# print
+# print
+# 	 - Aardappelen: 800 gr.
+# 	 - Wortelen: 500 gr.
+# 	 - erwten: 300 gr.
+# 	 - Worsten: 400 gr.
+
+
+# niveau2
+vraag_naar_hvl_man = int(input('voor hoeveel man wil je dit recept klaar maken '))
+schaal = vraag_naar_hvl_man / 4
+print("Recept voor gehakt met wortelen en erwten.")
+for sleutel, waarde in recept.items():
+    waarde = waarde * schaal     
+    print(sleutel,waarde)
+
+# Dit recept is bedoeld voor groepen van 4. Vraag aan de gebruiker voor hoeveel hij
+# dit gerecht wilt klaarmaken. Schaal de hoeveelheid eten naar dit aantal. Rond hoeveelheden altijd naar beneden af.
+
+# 	In de opdrachtprompt ziet dit er als volgt uit.
+# Voor hoeveel man kook je: 7
+# Recept voor gehakt met wortelen en erwten.
+#  - Aardappelen: 1400 gr.
+#  - Wortelen: 875 gr.
+#  - erwten: 525 gr.
+#  - Worsten: 700 gr.
