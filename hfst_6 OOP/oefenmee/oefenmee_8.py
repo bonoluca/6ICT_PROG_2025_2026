c2f78d2f8dd964f7df8ad14994cc3e471d16678d
bonoluca <bonoluca0707@gmail.com>
2026-04-28 08:50:47 +0200

    oefenmee 8 klaar

--- hfst_6 OOP/oefenmee/oefenmee_8.py
+++ hfst_6 OOP/oefenmee/oefenmee_8.py
@@ -1,5 +1,23 @@
+# Oefen mee 8
+# Maak een klasse Hond met de eigenschappen naam en massa. 
+class Hond:
+    klein = True
 
-" Via onderstaande code kan deze oefen mee testen. "
-# hond = Hond("Fido", 8)
-# paspoort = hond.genereer_paspoort()
-# print(paspoort) # Naam: Fido - Gewicht: 8 kg
+    def __init__(self, naam:str,  massa:float) -> None:
+        self.naam = naam
+        self.massa = massa
+    
+
+    def genereer_paspoort(self) -> str : 
+        passpoort = f'Naam : {self.naam} - {self.massa} kg'
+        return passpoort
+
+
+# Schrijf een methode genereer_paspoort(). Deze methode moet niet rekenen, maar een samengestelde tekst returnen: 
+# "Naam: *naam* - Gewicht: *massa* kg". 
+
+# Maak een object van Hond aan, roep de methode op en print de teruggegeven tekst.
+hond = Hond("Fido", 8)
+paspoort = hond.genereer_paspoort()
+print(paspoort) # Naam: Fido - Gewicht: 8 kg
+
