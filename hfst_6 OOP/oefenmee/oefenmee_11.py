672ec3741c1bcef326decc925c35c202a9c63d2f
bonoluca <bonoluca0707@gmail.com>
2026-06-02 08:33:27 +0200

    toets verbeteren

--- hfst_6 OOP/oefenmee/oefenmee_11.py
+++ hfst_6 OOP/oefenmee/oefenmee_11.py
@@ -1,29 +1,52 @@
 # Maak de klasse Persoon & Hond aan zoals omschreven in oefen mee 10.
 # Je start reeds met de __init__ van beide klassen.
 
-class Persoon:
-    def __init__(self, naam:str) -> None:
-        self.naam = naam
-        self.honden = []
-
+# Niveau 1
+# Voeg aan de klasse Persoon twee methoden toe (zaken tussen ** zijn eigenschappen):
+# Methode	Parameter(s)	Uitleg
+# koop_hond	self, hond	Voeg *naam* van hond toe aan *honden* persoon.
+# Verander *eigenaar* in hond naar *naam* persoon.
+# is_eigenaar	self, hond	Controleert of de persoon eigenaar is van de hond.
+# Return (niet print!) True of False.
 
 class Hond:
     def __init__(self, naam:str) -> None:
         self.naam = naam
         self.eigenaar = ""
 
+
+
+class Persoon:
+    def __init__(self, naam:str) -> None:
+        self.naam = naam
+        self.honden = []
+    
+    def koop_hond(self,hond:"Hond")->None:
+        self.honden.append(hond)
+        hond.eigenaar = self.naam
+
+    def is_eigenaar(self, hond:"Hond")->bool:
+        return self.naam == hond.eigenaar
+                     
+
+
+
+
+
+
+
 " Via onderstaande code kan je niveau 1 testen. "
 
-# hond_1 = Hond("Lulu")
-# hond_2 = Hond("Floris")
-# persoon_1 = Persoon("Jos")
-# persoon_2 = Persoon("Jef")
+hond_1 = Hond("Lulu")
+hond_2 = Hond("Floris")
+persoon_1 = Persoon("Jos")
+persoon_2 = Persoon("Jef")
 
-# persoon_1.koop_hond(hond_1)
-# persoon_2.koop_hond(hond_2)
+persoon_1.koop_hond(hond_1)
+persoon_2.koop_hond(hond_2)
 
-# print(persoon_1.is_eigenaar(hond_1)) # True
-# print(persoon_2.is_eigenaar(hond_1)) # False
+print(persoon_1.is_eigenaar(hond_1)) # True
+print(persoon_2.is_eigenaar(hond_1)) # False
 
 
 " Via onderstaande code kan je niveau 2 testen. "
